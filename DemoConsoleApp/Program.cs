eb183e9683e386ad26b321ef41ddf7e38c7a8446
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-06-07 12:20:28 +0530

    Add project files.

---
+++ DemoConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args) // SRP - UI
+        {
+            // Accept two int numbers and find the sum and display
+            // variables dec
+            int fno, sno, sum;
+            // IO Statements
+            Console.Write("Enter first number: ");
+            fno = int.Parse(Console.ReadLine());
+            Console.Write("Enter Second number: ");
+            sno = Convert.ToInt32(Console.ReadLine());
+            // find the sum
+            //sum = fno + sno;
+            sum = SimpleCalculatorClassLibrary.SimpleCalculator.FindSum(fno, sno);
+            // display
+            Console.WriteLine(sum);
+            Console.WriteLine("The sum is " + sum);
+            Console.WriteLine("The sum of {0} and {1} is {2}", fno, sno, sum);
+            Console.WriteLine($"The sum of {fno} and {sno} is {sum}");
+        } 
+
+       
+    } // UI
+
+    
+}
---
+++ SimpleCalculatorClassLibrary/SimpleCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculatorClassLibrary
+{
+    public class SimpleCalculator // BL
+    {
+        public static int FindSum(int a, int b) // BL
+        {
+            return a + b;
+        }
+    }
+}
