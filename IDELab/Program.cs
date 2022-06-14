27305c9a8625d5af767807c327aa1865172fba6b
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-06-14 09:27:06 +0530

    oo

---
+++ IDELab/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDELab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IDE i = new IDE();
+            LangC c = new LangC();
+            LangCSharp cs = new LangCSharp();
+            LangJava java = new LangJava();
+            LangVBNet vb = new LangVBNet();
+            i.Languages.Add(c);
+            i.Languages.Add(cs);
+            i.Languages.Add(vb);
+            i.Languages.Add(java);
+
+            i.DoWork();
+
+        }
+    }
+
+    public interface ILanguage
+    {
+        string GetName();
+        string GetUnit();
+        string GetParadigm();
+    }
+
+    abstract public class ObjectOriented : ILanguage
+    {
+        abstract public string GetName();
+      
+
+        public string GetParadigm()
+        {
+            return "OOP";
+        }
+
+        public string GetUnit()
+        {
+            return "Class";
+        }
+    }
+
+    abstract public class Procedural : ILanguage
+    {
+        public string GetUnit()
+        {
+            return "Function";
+        }
+        public string GetParadigm()
+        {
+            return "Procedural";
+        }
+
+        abstract public string GetName();
+        
+    }
+
+    class IDE // OCP
+    {
+        //public LangC C { get; set; }
+        //public LangCSharp CS { get; set; }
+        //public LangJava Java { get; set; }
+
+        public List<ILanguage> Languages { get; set; } = new List<ILanguage>();
+
+
+        public void DoWork()
+        {
+            foreach (ILanguage language in Languages)
+            {
+            Console.WriteLine(language.GetName());
+            Console.WriteLine(language.GetUnit());
+            Console.WriteLine(language.GetParadigm());
+            }
+        }
+    }
+
+    class LangC :  Procedural
+    {
+        override public string GetName()
+        {
+            return "C Language";
+        }
+       
+    }
+    
+    class LangCSharp : ObjectOriented
+    {
+        override public string GetName()
+        {
+            return "C# Language";
+        }
+       
+    }
+
+    class LangJava : ObjectOriented
+    {
+        override public string GetName()
+        {
+            return "Java Language";
+        }
+       
+    }
+
+    class LangVBNet : ObjectOriented
+    {
+        override public string GetName()
+        {
+            return "VB.Net";
+        }
+
+      
+    }
+}
---
+++ OODemo2/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //IShape s;//= new Shape { H = 10, W = 15 };
+
+             //Shape s = new Rectangle(); // Base class ref variable can hold derived type objects
+            IShape s = new Triangle();
+            //Line s = new Line();
+            s.Draw();
+            //Draw(s);
+
+        }
+
+        static void Draw(IShape s)
+        {
+            s.Draw();
+        }
+    }
+
+    class Line : IShape
+    {
+        public int H { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int W { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void CalculateArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class ThreeDLine : Line
+    {
+        
+    }
+
+     public interface IShape
+    {
+         int H { get; set; }
+         int W { get; set; }
+
+         void CalculateArea();
+        //{
+            
+        //    Console.WriteLine($"The area of Shape is {H*W}");
+        //}
+
+         void Draw();
+        //{
+        //    Console.WriteLine("Drawing Shape");
+        //}
+    }
+
+    class Rectangle : IShape // IS-A - Realization
+    {
+        public  int H { get; set;}
+        public  int W { get; set ; }
+
+        public  void CalculateArea()
+        {
+            Console.WriteLine(H*W);
+        }
+
+        public  void Draw()
+        {
+            Console.WriteLine("Drawing Ractangle");
+        }
+    }
+
+    class Triangle : IShape
+    {
+        public int H { get; set ; }
+        public int W { get ; set ; }
+
+        public void CalculateArea()
+        {
+            Console.WriteLine(H*W/2);
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine("Drawing Traingle");
+        }
+    }
+}
