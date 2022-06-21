c0a5bee48650852be1eae626921dd0dd4acef158
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-06-21 12:28:59 +0530

    file demos

--- FileIODemo2/Program.cs
+++ FileIODemo2/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
 
 namespace FileIODemo2
 {
@@ -11,18 +13,78 @@ namespace FileIODemo2
     {
         static void Main(string[] args)
         {
-
             //Product p = new Product { ProductID = 111, Name = "IPhone X", Cost = 75000, Brand = "Apple" };
+            //XmlSerializeProduct(p);
+
+            Product product = XmlDeserializeProduct();
+            Console.WriteLine($"{product.ProductID}\t{product.Name}\t{product.Cost}\t{product.Brand}");
+
+            //Product product = DeserializeProduct();
+            //Console.WriteLine($"{product.ProductID}\t{product.Name}\t{product.Cost}\t{product.Brand}");
+            //Product p = new Product { ProductID = 111, Name = "IPhone X", Cost = 75000, Brand = "Apple" };
+            //SerializeProduct(p);
+            //Console.WriteLine("Product is serialized");
             //SaveProduct(p);
             //Console.WriteLine("Product saved");
 
-            List<Product> products = ReadAllProducts();
-            // display
-            Console.WriteLine("Id\tName\tCost\tBrand");
-            foreach (Product product in products)
-            {
-                Console.WriteLine($"{product.ProductID}\t{product.Name}\t{product.Cost}\t{product.Brand}");
-            }
+            //List<Product> products = ReadAllProducts();
+            //// display
+            //Console.WriteLine("Id\tName\tCost\tBrand");
+            //foreach (Product product in products)
+            //{
+            //    Console.WriteLine($"{product.ProductID}\t{product.Name}\t{product.Cost}\t{product.Brand}");
+            //}
+
+        }
+
+
+        static Product DeserializeProduct()
+        {
+            Stream stream = new FileStream("e:\\products.dat", FileMode.Open);
+            BinaryFormatter binary = new BinaryFormatter();
+            Product p = (Product) binary.Deserialize(stream);
+            return p;
+        }
+
+
+        static Product XmlDeserializeProduct()
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(Product));
+            Stream stream = new FileStream("e:\\products.xml", FileMode.Open);
+            Product p = (Product)xml.Deserialize(stream);
+            return p;
+        }
+        static void XmlSerializeProduct(Product p)
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(Product));
+            Stream stream = new FileStream("e:\\products.xml", FileMode.Append);
+            xml.Serialize(stream, p);
+            stream.Close();
+        }
+
+        static void SerializeProduct(Product p)
+        {
+
+            Stream stream = new FileStream("e:\\products.dat", FileMode.Append);
+            BinaryFormatter binary = new BinaryFormatter();
+            binary.Serialize(stream, p);
+            stream.Close();
+            // Binary
+            // XML
+            // SOAP
+            // JSON
+
+
+            //StreamWriter writer = new StreamWriter(@"E:\products.txt", true);
+            //try
+            //{
+            //    //string csvData = $"{p.ProductID},{p.Name},{p.Cost},{p.Brand}";
+            //    //writer.WriteLine(csvData);
+            //}
+            //finally
+            //{
+            //    writer.Close();
+            //}
         }
 
         static void SaveProduct(Product p)
@@ -63,7 +125,8 @@ namespace FileIODemo2
         }
     }
 
-    class Product
+    [Serializable]
+    public class Product
     {
         public int ProductID { get; set; }
         public string Name { get; set; }
---
+++ FileIODemo3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIODemo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //GetAllDrives();
+            //GetAllFiles();
+
+            //File.Copy()
+            Directory.CreateDirectory("");
+            File.Delete("");
+            File.Encrypt("sdf");
+            File.Decrypt("dfsd");
+            File.Move("", "");
+            File.Exists("");
+
+
+        }
+
+
+        static void GetAllFiles()
+        {
+            string[] files = Directory.GetFiles(@"E:\Training Materials 2020\A");
+            foreach (string f in files)
+            {
+                Console.WriteLine(f);
+            }
+
+        }
+        static void GetAllDrives()
+        {
+
+           DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine(drive.Name);
+                
+            }
+        }
+
+    }
+}
