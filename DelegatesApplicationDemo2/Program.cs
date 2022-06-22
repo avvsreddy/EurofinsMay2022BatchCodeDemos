68e1a0b2bad21a7891321b87fdb65e76a2b90c35
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-06-22 13:09:03 +0530

    delegates

--- DelegatesApplicationDemo2/Program.cs
+++ DelegatesApplicationDemo2/Program.cs
@@ -12,43 +12,61 @@ namespace DelegatesApplicationDemo2
         static void Main(string[] args)
         {
             // client 1
-            //ProcessManager.ShowProcessList();
+            ProcessManager.ShowProcessList(NoFilter);
             // client 2
-            //ProcessManager.ShowProcessList("s");
+            //FilterDelegate filter = new FilterDelegate(FilterByName);
+            //ProcessManager.ShowProcessList(filter);
             // client 3
-            ProcessManager.ShowProcessList(50 * 1024 * 1024);
+            //ProcessManager.ShowProcessList(FilterByMemSize);
 
 
+        }
+
+        // client 1
+        public static bool NoFilter(Process p)
+        {
+            return true;
+        }
+        //client3
+        public static bool FilterByMemSize(Process p)
+        {
+            return p.WorkingSet64 >= 50 * 1024 * 1024;
+        }
+        // client 2
+        public static bool FilterByName(Process p)
+        {
+            return p.ProcessName.StartsWith("S");
         }
     }
 
+    public delegate bool FilterDelegate(Process p);
     class ProcessManager
     {
         // c1
-        public static void ShowProcessList()
-        {
-            foreach (Process p in Process.GetProcesses())
-            {
-                Console.WriteLine(p.ProcessName);
-            } 
-        }
+        //public static void ShowProcessList()
+        //{
+        //    foreach (Process p in Process.GetProcesses())
+        //    {
+        //        Console.WriteLine(p.ProcessName);
+        //    } 
+        //}
         // c2
-        public static void ShowProcessList(string sw)
+        public static void ShowProcessList(FilterDelegate filter)
         {
             foreach (Process p in Process.GetProcesses())
             {
-                if(p.ProcessName.StartsWith(sw))
+                if(filter(p))
                     Console.WriteLine(p.ProcessName);
             }
         }
         // c3
-        public static void ShowProcessList(long memSize)
-        {
-            foreach (Process p in Process.GetProcesses())
-            {
-                if (p.WorkingSet64 >= memSize)
-                    Console.WriteLine(p.ProcessName);
-            }
-        }
+        //public static void ShowProcessList(long memSize)
+        //{
+        //    foreach (Process p in Process.GetProcesses())
+        //    {
+        //        if (p.WorkingSet64 >= memSize)
+        //            Console.WriteLine(p.ProcessName);
+        //    }
+        //}
     }
 }
