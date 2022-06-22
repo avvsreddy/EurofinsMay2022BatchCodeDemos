99bd8345f5361fa44aeb9423aa220a21c36a2077
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-06-22 11:46:38 +0530

    delegates

---
+++ DelegatesApplicationDemo/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesApplicationDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account();
+            acc1.notification = Alert.SendMail; // subscribe
+            acc1.notification += Alert.SendSMS; // subscribe
+            acc1.notification.Invoke("EMAIL : Your account has been deposited $999999999999999999999");
+            //acc1.Deposit(1000);
+            //acc1.Withdraw(100);
+        }
+    }
+
+    class Account
+    {
+        public int Balance { get; private set; }
+        private Notification notification = null; //new Notification(Alert.SendMail);
+
+        public void Deposit(int amount)
+        {
+            Balance += amount;
+            if (notification != null)
+            {
+                notification($"Deposited : {amount}");
+            }
+            //Alert.SendMail($"Deposited : {amount}");
+            //Alert.SendSMS($"Deposited : {amount}");
+            //Console.WriteLine($"EMAIL - Deposited : {amount}");
+        }
+        public void Withdraw(int amount)
+        {
+            Balance -= amount;
+            if (notification != null)
+            {
+                notification($"Withdran:{ amount}");
+            }
+            //Console.WriteLine($"EMAIL - Withdran :{amount}");
+            //Alert.SendMail($"Withdran:{ amount}");
+            //Alert.SendSMS($"Withdran:{ amount}");
+        }
+    }
+
+    // declare a delegate
+    delegate void Notification(string str);
+
+
+    class Alert
+    {
+        public static void SendMail(string msg)
+        {
+            Console.WriteLine("Email: " + msg);
+        }
+
+        public static void SendSMS(string msg)
+        {
+            Console.WriteLine("SMS :" + msg);
+        }
+    }
+}
---
+++ DelegatesDemo1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesDemo1
+{
+
+    //class MyDelegate : MulticastDelegate
+    //{
+
+    //}
+
+    delegate void MyDelegate(string str); // Delegate Declaration
+    delegate void MyDelegate2();
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Direct
+            //Greeting("Hello 1");
+            // Indirect
+            //MulticastDelegate mcd = new
+            Program p = new Program();
+            MyDelegate md = new MyDelegate(p.Hello);
+            md += Greeting; // Subscription
+            //md.Invoke("Hello 2");
+            md -= p.Hello;
+            MyDelegate2 md2 = new MyDelegate2(Hi);
+            //md += Hi;
+            //md -= Greeting;
+            if(md != null)
+                md("Hi...");
+        }
+
+        static void Hi()
+        {
+            Console.WriteLine("Hi");
+        }
+
+        static void Greeting(string msg)
+        {
+            Console.WriteLine($"Greeting :{msg}");
+        }
+
+        void Hello(string str)
+        {
+            Console.WriteLine($"Hello :{str}");
+        }
+    }
+}
