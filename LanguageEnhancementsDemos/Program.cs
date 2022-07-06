eceda1ec5863825567702f718efdd4dd3940c5d2
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-07-06 12:05:13 +0530

    linq

---
+++ LanguageEnhancementsDemos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageEnhancementsDemos
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            // Local Variable type inference
+            // Object Initialization Syntax
+            var p1 = new  { Id = 111, Name = "aaaa" };
+            //p1.Id = 222;
+            // Anonymous Types
+            // Extension Methods
+            // Lambda
+            // Automatic Properties
+
+            string data = "some data";
+            //data.ToUpper();
+            data.Encrypt();
+            data = StringEncrypt.Encrypt(data);
+            int a = 100;
+            a.Encrypt();
+            Program p = new Program();
+            p.Encrypt();
+        }
+    }
+
+    public static class StringEncrypt
+    {
+        public static string Encrypt(this object input)
+        {
+            return "@#$@#RSDFSDFS@#$@#$@#SDFSF@#$@#$@#";
+        }
+
+        
+    }
+
+
+    //class Anoymous234234234
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
---
+++ Linq2ObjectsDemo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Linq2ObjectsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Linq to Objects
+
+            int[] numbers = { 11, 52, 36, 14, 65, 46, 77, 18, 29 };
+
+            // Table numbers
+            // column number
+
+            // sql: select number from numbers where number mod 2 = 0 order by number desc;
+
+            // extract all even numbers into another array in decreasing order
+            // Linq to Object: 
+
+            var evenNumbers = from n in numbers 
+                              where n % 2 == 0 
+                              orderby n descending
+                              select n;
+
+
+            // Linq to XML
+            XDocument xml = XDocument.Load("XMLFile1.xml");
+
+            var evensFromXml = from n in xml.Descendants("number")
+                               where int.Parse(n.Value) % 2 == 0
+                               orderby n.Value descending
+                               select n.Value;
+
+            foreach (var item in evensFromXml)
+            {
+                Console.WriteLine(item);
+            }
+
+            int[] evens = new int[numbers.Length];
+            for (int i = 0,x=0; i < numbers.Length; i++)
+            {
+                if(numbers[i] % 2 == 0)
+                {
+                    evens[x++] = numbers[i];
+                }
+            }
+
+            // display all data in a new array
+            //foreach (var item in evens)
+            //{
+            //    Console.WriteLine(item);
+            //}
+        }
+    }
+}
