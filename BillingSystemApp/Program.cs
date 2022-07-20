55efbd34ffb28580f24e973b4ba2b47396f559b1
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-07-20 11:08:33 +0530

    threads

---
+++ BillingSystemApp/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystemApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BillingSystem billingSystem = new BillingSystem(TaxCalculatorFactory.Create());
+            billingSystem.GenerateBill();
+        }
+    }
+
+
+    class  TaxCalculatorFactory
+    {
+        public static ITaxCalculator Create()
+        {
+
+            string taxCalc = ConfigurationManager.AppSettings["TAX"];
+            Type theType = Type.GetType(taxCalc);
+            return (ITaxCalculator)Activator.CreateInstance(theType);
+            //return new APTaxCalculator();
+        }
+    }
+
+    public class BillingSystem
+    {
+        private ITaxCalculator taxCalculator = null;
+
+        public BillingSystem(ITaxCalculator taxCalculator)
+        {
+            this.taxCalculator = taxCalculator;
+        }
+        public void GenerateBill()
+        {
+            // calculate the bill amount
+            int billAmount = 5600;
+            // calculate the tax
+            //ITaxCalculator calculator = new APTaxCalculator();
+            int tax = taxCalculator.CalculateTax(billAmount);
+            // calculate the discount
+            // generate the bill
+        }
+    }
+
+    public interface ITaxCalculator
+    {
+        int CalculateTax(int amount);
+    }
+
+    public class KATaxCalculator : ITaxCalculator
+    {
+        public int CalculateTax(int amount)
+        {
+            int totalTax = 0;
+            int es = 100;
+            int kst = 120;
+            int cst = 120;
+            int sbt = 50;
+            int ft = 75;
+            int it = 20;
+            totalTax = es + kst + cst + sbt + ft + it;
+            Console.WriteLine("Using KA Tax Calculator");
+            return totalTax;
+        }
+    }
+
+    public class APTaxCalculator : ITaxCalculator
+    {
+        public int CalculateTax(int amount)
+        {
+            int totalTax = 0;
+            int es = 100;
+            int kst = 120;
+            int cst = 120;
+            int sbt = 150;
+            int ft = 75;
+           
+            totalTax = es + kst + cst + sbt + ft ;
+            Console.WriteLine("Using AP Tax Calculator");
+            return totalTax;
+        }
+    }
+
+    public class UPTaxCalculator : ITaxCalculator
+    {
+        public int CalculateTax(int amount)
+        {
+            int totalTax = 0;
+            int es = 100;
+            int kst = 120;
+            int cst = 120;
+            int sbt = 150;
+            int ft = 75;
+
+            totalTax = es + kst + cst + sbt + ft;
+            Console.WriteLine("Using UP Tax Calculator");
+            return totalTax;
+        }
+    }
+
+
+
+}
---
+++ MTDemo1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace MTDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main running in thread :{Thread.CurrentThread.ManagedThreadId}");
+            Stopwatch w = Stopwatch.StartNew();
+            Console.WriteLine("Running Seq....");
+            M1();
+            M2();
+            Console.WriteLine($"Time taken to complete {w.ElapsedMilliseconds}");
+            Console.WriteLine("Running in multiple threads");
+            w = Stopwatch.StartNew();
+            // using Thread class
+            ThreadStart ts1 = new ThreadStart(M1);
+            //ThreadStart ts2 = new ThreadStart(M2);
+
+            Thread t1 = new Thread(ts1);
+            Thread t2 = new Thread(M2);
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();
+            Console.WriteLine($"Time taken to complete {w.ElapsedMilliseconds}");
+            // using TPL
+            w = Stopwatch.StartNew();
+            Console.WriteLine("Running using TPL Parallel class");
+            Parallel.Invoke(M1, M2);
+            Console.WriteLine($"Time taken to complete {w.ElapsedMilliseconds}");
+            w = Stopwatch.StartNew();
+            Console.WriteLine("Running using  TPL Task class");
+            Task tt1 = new Task(M1);
+            Task tt2 = new Task(M2);
+            tt1.Start();
+            tt2.Start();
+            //Task.WaitAll();
+            tt1.Wait();
+            tt2.Wait();
+            Console.WriteLine($"Time taken to complete {w.ElapsedMilliseconds}");
+            w = Stopwatch.StartNew();
+            Console.WriteLine("Running using  TPL Parallel Loop ");
+            Parallel.Invoke(M11, M22);
+            Console.WriteLine($"Time taken to complete {w.ElapsedMilliseconds}");
+        }
+
+        static void M1()
+        {
+            //Console.WriteLine($"M1 running in thread :{Thread.CurrentThread.ManagedThreadId}");
+            // complex logic 
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread.Sleep(100);
+            }
+            
+        }
+        static void M2()
+        {
+            //Console.WriteLine($"M2 running in thread :{Thread.CurrentThread.ManagedThreadId}");
+            // complex logic 
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread.Sleep(100);
+            }
+            
+        }
+
+        static void M11()
+        {
+            //Console.WriteLine($"M1 running in thread :{Thread.CurrentThread.ManagedThreadId}");
+            // complex logic 
+            //for (int i = 1; i <= 10; i++)
+            Parallel.For(1, 11, i => 
+            {
+                Thread.Sleep(100);
+            });
+
+        }
+        static void M22()
+        {
+            //Console.WriteLine($"M2 running in thread :{Thread.CurrentThread.ManagedThreadId}");
+            // complex logic 
+            Parallel.For(1, 11, i =>
+            {
+                Thread.Sleep(100);
+            });
+
+        }
+    }
+}
